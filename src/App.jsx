ab7287b06090a35d10c2bddce8c7c1cb579fab15
Savina Daria <savina.dariia@chnu.edu.ua>
2024-11-24 23:38:38 +0200

    [4.1] vacancy page -- move company logo down and left, change text

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,12 @@ import NoPage from "/src/pages/NoPage";
 import Landing from "/src/pages/Landing";
 import Blog from "/src/pages/Blog";
 import Post from "src/pages/Post.jsx";
+import Vacancy from "src/pages/Vacancy.jsx";
 
 export default function App() {
   return (
     <>
-      <Post
-          header="The Ultimate Guide to React Component Styling"
-          username="Jane Doe"
-          likes={124}
-          comments={32}
-          bannerImage="https://via.placeholder.com/800x400" // Replace with actual image URL
-          tags={['React', 'CSS', 'JavaScript', 'Frontend']}
-      />
+      <Vacancy/>
     </>
   )
 }
